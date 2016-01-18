2f4f5aae65d57c415e5d8bfc9b248fcaa504b437
Robert Knight <robertknight@gmail.com>
2016-01-18 08:20:54 +0000

    Small Git and Fish config updates
     - Update email address
     - Add custom prompt for when working in a virtualfish environment

--- dotfiles/fish_prompt.fish
+++ dotfiles/fish_prompt.fish
@@ -1,11 +1,11 @@
-# add symlink to this under
-# ~/.config/fish/functions
-
 function fish_prompt
 	set job_list (jobs -c)
 	set pwd_section (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 	if [ (count $job_list) -gt 0 ]
 		set job_section "($job_list)"
+	end
+	if set -q VIRTUAL_ENV
+		echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
 	end
 	printf '%s%s> ' "$pwd_section" "$job_section"
 end
