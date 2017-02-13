bcc13462b09c7deef418802010e83ad185ad9166
Robert Knight <robertknight@gmail.com>
2017-02-13 12:04:11 +0000

    Add language-specific binary dirs to path

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,7 +1,24 @@
 # Get path to this script, assuming it loaded via a symlink.
 set SCRIPT_DIR (dirname (readlink (status --current-filename)))
 
-set -x PATH $HOME/projects/go/bin $HOME/other/go/bin $PATH
+# Helpers
+function add_dir_to_path
+  set dir $argv[1]
+  if test -d $dir
+    set -x PATH $dir $PATH
+  end
+end
+
+function eval_if_exists
+  set path $argv[1]
+  if test -f $path
+    source $path
+  end
+end
+
+# Language-specific binary dirs
+add_dir_to_path $HOME/projects/go/bin
+add_dir_to_path $HOME/other/go/bin
 
 # Setup virtualfish for Python
 eval (python -m virtualfish)
@@ -10,3 +27,5 @@ eval (python -m virtualfish)
 # (https://github.com/idan/autoenvfish)
 set AUTOENV_SRC $SCRIPT_DIR/../vendor/fish/autoenv.fish
 source $AUTOENV_SRC
+
+eval_if_exists /usr/local/share/autojump/autojump.fish
