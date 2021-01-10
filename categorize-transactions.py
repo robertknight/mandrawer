2f8f526f44dd9353bd94a8d156d25e52b7193102
Robert Knight <robertknight@gmail.com>
2021-01-10 19:40:02 +0000

    Add features to print transaction totals and filter by category
     - Print total debit/credit amount for each transaction
       description
    
     - Add `--list-categories` option
    
     - Add `--select-categories` option to filter transactions to those
       which match a given category

--- categorize-transactions.py
+++ categorize-transactions.py
@@ -82,7 +82,7 @@ def parse_lloyds_statement(statement_csv_file):
 
 
 def parse_categories(categories_json):
-    category_rules = json.load(categories_json)
+    category_rules = json.load(categories_json)["categories"]
 
     categories = []
     for cat_json in category_rules:
@@ -93,14 +93,51 @@ def parse_categories(categories_json):
     return categories
 
 
+def category_matches(transaction, categories):
+    return [cat for cat in categories if cat.matches(transaction)]
+
+
 if __name__ == "__main__":
     parser = ArgumentParser(description="Bank transaction analyzer")
     parser.add_argument("categories", help="JSON file containing categories")
     parser.add_argument("transactions", help="CSV file containing transactions")
+    parser.add_argument(
+        "--list-categories",
+        dest="list_categories",
+        action="store_true",
+        help="List transaction categories",
+    )
+    parser.add_argument(
+        "--select-categories",
+        dest="select_categories",
+        type=str,
+        help="Comma-separated list of transaction categories to include",
+    )
     args = parser.parse_args()
 
     transactions = parse_lloyds_statement(open(args.transactions))
     categories = parse_categories(open(args.categories))
+
+    category_from_name = {}
+    for cat in categories:
+        category_from_name[cat.name] = cat
+
+    if args.list_categories:
+        cat_names = sorted(cat.name for cat in categories)
+        for name in cat_names:
+            print(name)
+        sys.exit(0)
+
+    # Filter transactions by category if `--select-categories` is specified.
+    if args.select_categories:
+        filter_categories = set(
+            category_from_name[cat] for cat in args.select_categories.split(",")
+        )
+        transactions = [
+            tx
+            for tx in transactions
+            if set(category_matches(tx, categories)) & filter_categories
+        ]
 
     # Classify transactions.
     category_transactions = {}
@@ -108,7 +145,7 @@ if __name__ == "__main__":
     multiple_category_tx_descriptions = {}
 
     for tx in transactions:
-        cat = [cat for cat in categories if cat.matches(tx)]
+        cat = category_matches(tx, categories)
         if len(cat) > 1:
             multiple_category_tx_descriptions[tx.description] = cat
 
@@ -138,9 +175,9 @@ if __name__ == "__main__":
     if len(unknown_tx_descriptions):
         print(f"\n{len(unknown_tx_descriptions)} transactions with unknown category:")
         for desc in sorted(unknown_tx_descriptions.keys()):
-            transactions = unknown_tx_descriptions[desc]
-            debit_total = sum(tx.debit_amount for tx in transactions)
-            credit_total = sum(tx.credit_amount for tx in transactions)
+            txs = unknown_tx_descriptions[desc]
+            debit_total = sum(tx.debit_amount for tx in txs)
+            credit_total = sum(tx.credit_amount for tx in txs)
 
             totals = []
             if debit_total > 0:
@@ -150,14 +187,40 @@ if __name__ == "__main__":
 
             print(f"  {desc}: {', '.join(totals)}")
 
+    # Print transaction totals.
+    print(f"\nTransaction totals by sender/receiver:")
+    tx_descriptions = set(tx.description for tx in transactions)
+    for desc in sorted(tx_descriptions):
+        txs = [tx for tx in transactions if tx.description == desc]
+        txs = sorted(txs, key=lambda tx: tx.date)
+
+        debit_total = sum(tx.debit_amount for tx in txs)
+        credit_total = sum(tx.credit_amount for tx in txs)
+
+        totals = []
+        if debit_total > 0:
+            totals.append(f"{debit_total} out")
+        if credit_total > 0:
+            totals.append(f"{credit_total} in")
+
+        first_tx_date = txs[0].date.strftime("%d/%m/%y")
+        last_tx_date = txs[-1].date.strftime("%d/%m/%y")
+
+        if first_tx_date == last_tx_date:
+            date_range = first_tx_date
+        else:
+            date_range = f"{first_tx_date}-{last_tx_date}"
+
+        print(f"  {desc} ({len(txs)}): {', '.join(totals)}. {date_range}")
+
     # Print category totals.
     print(f"\nCategory totals:")
     cat_names = sorted(category_transactions.keys())
     for cat_name in cat_names:
-        transactions = category_transactions[cat_name]
+        cat_txs = category_transactions[cat_name]
 
-        debit_total = sum(tx.debit_amount for tx in transactions)
-        credit_total = sum(tx.credit_amount for tx in transactions)
+        debit_total = sum(tx.debit_amount for tx in cat_txs)
+        credit_total = sum(tx.credit_amount for tx in cat_txs)
 
         totals = []
         if debit_total > 0:
