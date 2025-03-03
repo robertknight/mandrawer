41dec9974fbe155da2e7a7c1bd08c321cd1d4f26
Robert Knight <robertknight@gmail.com>
2025-03-03 07:02:50 +0000

    Update setup.py for Python 3, add Neovim setup

--- dotfiles/setup.py
+++ dotfiles/setup.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from __future__ import print_function
-
-import distutils.dir_util
 import glob
+from pathlib import Path
 import os.path
 
 
@@ -12,19 +10,20 @@ HOME_DIR = os.environ['HOME']
 UNIVERSAL_CTAGS_CONFIG_DIR = os.path.join(HOME_DIR, '.ctags.d')
 FISH_CONFIG_DIR = os.path.join(HOME_DIR, '.config/fish')
 FISH_FUNC_DIR = os.path.join(FISH_CONFIG_DIR, 'functions')
+NVIM_CONFIG_DIR = os.path.join(HOME_DIR, '.config/nvim')
 
 
 def yes_no_prompt(prompt):
-    result = raw_input('%s [Y/N]\n' % prompt)
+    result = input('%s [Y/N]\n' % prompt)
     return result.lower() in ['y', 'yes']
 
 
 def install_symlink(src, dest_dir):
-    distutils.dir_util.mkpath(dest_dir)
+    Path(dest_dir).mkdir(parents=True, exist_ok=True)
     dest_path = os.path.join(dest_dir, os.path.basename(src))
     abs_src_path = os.path.join(DOTFILE_DIR, src)
     if os.path.lexists(dest_path):
-        if os.readlink(dest_path) == abs_src_path:
+        if os.path.islink(dest_path) and os.readlink(dest_path) == abs_src_path:
             # Symlink already exists and points to target file.
             return
         if not yes_no_prompt('Replace existing file \'%s\'?' % dest_path):
@@ -44,6 +43,10 @@ def setup_ctags():
     install_symlink('config.ctags', UNIVERSAL_CTAGS_CONFIG_DIR)
 
 
+def setup_neovim():
+    install_symlink('init.vim', NVIM_CONFIG_DIR)
+
+
 def symlink_dotfiles():
     dotfiles = glob.glob('.*')
     for dotfile in dotfiles:
@@ -54,6 +57,7 @@ def main():
     symlink_dotfiles()
     setup_ctags()
     setup_fish()
+    setup_neovim()
 
 
 if __name__ == '__main__':
